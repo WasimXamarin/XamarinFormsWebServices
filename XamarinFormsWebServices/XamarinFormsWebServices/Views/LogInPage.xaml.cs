c207ffd8ec40175c1f73d4f10f969306c00a9fea
WASIM <wasimalam@virtualemployee.com>
2022-02-03 15:04:40 +0530

    Folder Add
    Folder Structure Add

---
+++ XamarinFormsWebServices/XamarinFormsWebServices/Views/LogInPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsWebServices.Views
+{
+    public partial class LogInPage : ContentPage
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
