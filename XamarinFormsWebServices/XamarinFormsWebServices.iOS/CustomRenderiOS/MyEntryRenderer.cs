a90fd8ca45e331eeee41c829a0c633c0dafa8172
WASIM <wasimalam@virtualemployee.com>
2022-02-09 22:42:26 +0530

    Page Color Change
    Page Color Change

---
+++ XamarinFormsWebServices/XamarinFormsWebServices.iOS/CustomRenderiOS/MyEntryRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinFormsWebServices.CustomRender;
+using XamarinFormsWebServices.iOS.CustomRenderiOS;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace XamarinFormsWebServices.iOS.CustomRenderiOS
+{
+    public class MyEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if(Control != null)
+            {
+                Control.BackgroundColor = UIColor.FromRGB(204, 153, 255);
+                Control.BorderStyle = UITextBorderStyle.None;
+            }
+        }
+    }
+}
---
+++ XamarinFormsWebServices/XamarinFormsWebServices/Models/LogInModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace XamarinFormsWebServices.Models
+{
+    public class LogInModel
+    {
+        
+    }
+}
